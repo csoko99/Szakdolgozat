a418f1fb134c655f0865794fc82c286eb7daec74
csoko99 <laczkolali77@gmail.com>
2024-10-12 18:46:44 +0100

    fix path to views

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const port = process.env.PORT || 3002;
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('views'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'oraterv.html'));
+  res.sendFile(path.join(__dirname, 'views', 'oraterv.html'));
 });
 
 // Routes
